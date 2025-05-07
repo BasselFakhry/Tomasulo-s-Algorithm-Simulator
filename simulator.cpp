e51ef8148454461c095208d583b5f0b5817c57f5
BasselFakhry <115945638+BasselFakhry@users.noreply.github.com>
2025-05-07 20:59:23 +0300

    Update simulator.cpp

--- simulator.cpp
+++ simulator.cpp
@@ -11,13 +11,16 @@ private:
     std::string opcode;
     std::string r1, r2, r3;
     std::string offset;
+    std::string current_state;
     size_t instruction_count;
     size_t start;
     size_t end;
     size_t duration;
     size_t stalls;
+
 public:
-    Instruction(std::string instruction, size_t instruction_count): instruction_count(instruction_count),duration(0), stalls(0){
+
+    Instruction(std::string instruction, size_t instruction_count): current_state("Out"), instruction_count(instruction_count),duration(0), stalls(0){
         std::istringstream iss(instruction);
             std::string word;
 
@@ -166,13 +169,10 @@ class Reservation_station{
         std::vector<Register> src2;
         
     public:
-        Reservation_station(size_t capacity): capacity(capacity), occupancy(0), busy(false), src1(capacity), src2(capacity){
-            for(size_t i = 0; i < capacity; ++i){
-                this->src1[i] = Register(0);
-                this->src2[i] = Register(0);
-            }
-                   
+        Reservation_station(size_t capacity): capacity(capacity), occupancy(0), busy(false), src1(capacity,Register(false)), src2(capacity,Register(false)){
+                
         }
+
         size_t addInstruction(Register src1, Register src2){
             assert(!isFull());
             for(size_t i = 0; i < capacity; ++i){
@@ -209,7 +209,7 @@ private:
     Reservation_station reservation_station;
 
 public:
-    Alu(size_t latency, size_t capacity, Reservation_station Reservation_station):latency(latency), capacity(capacity), pipeline(latency,0), reservation_station(reservation_station){
+    Alu(size_t latency, size_t capacity, Reservation_station reservation_station):latency(latency), capacity(capacity), pipeline(latency,0), reservation_station(reservation_station){
 
     }
 
@@ -257,7 +257,7 @@ public:
 
 class Tomasulo_simulator{
 private:
-    std::queue<Instruction> instructions;
+    std::vector<Instruction> instructions;
     Alu alu1;
     Alu alu2;
     size_t num_instructions;
@@ -277,7 +277,7 @@ private:
     
 
 public:
-    Tomasulo_simulator(std::string code): alu1(0, 0, Reservation_station(0)), alu2(0, 0, Reservation_station(0)), multiplier_divider(0, 0), memory(0, 0){
+    Tomasulo_simulator(std::string code): alu1(2, 2, Reservation_station(2)), alu2(1, 1, Reservation_station(1)), multiplier_divider(0, 0), memory(0, 0){
         this->num_instructions = 0;
         this->cycle = 0;
         this->add_sub_time = 2;
@@ -289,7 +289,7 @@ public:
         this->div_capacity = 2;
         this->load_store_capacity = 4;
         this->register_file = Register_file();
-        Reservation_station alu_shared_rs (add_sub_capacity);
+        Reservation_station alu_shared_rs (this->add_sub_capacity);
         this->alu1 = Alu(add_sub_time, add_sub_capacity, alu_shared_rs);
         this->alu2 = Alu(mul_time, mul_capacity, alu_shared_rs);
         this->multiplier_divider = Multiplier_Divider(mul_time, mul_capacity);
@@ -300,7 +300,7 @@ public:
         std::string line;
         if (file.is_open()) {
             while (std::getline(file, line)) {
-                this->instructions.push(Instruction(line, this->num_instructions ++));
+                this->instructions.push_back(Instruction(line, this->num_instructions ++));
             }
             file.close();
         }else {
@@ -308,15 +308,16 @@ public:
         }
                 
     }
+
+
     void print(){
         std::cout << "Num of instructions " << this->num_instructions << std::endl;
         
-        std::queue<Instruction> instructions_copy = this->instructions;
-        while (!instructions_copy.empty()) {
-            instructions_copy.front().print();
-            instructions_copy.pop();
-
+        
+        for(size_t i = 0; i < this->num_instructions; ++i){
+            this->instructions[i].print();
         }
+        
     }
 
     void print_report(){
@@ -324,7 +325,7 @@ public:
         std::cout << "Num of instructions " << this->num_instructions << std::endl;
         std::cout << "Total execution time = " << this-> cycle <<" clock cycles." << std::endl;
         std::cout << "Average number of instructions per cycle = " << this->cycle / this->num_instructions << " (IPC)." << std::endl;
-        
+
     }
 };
 
